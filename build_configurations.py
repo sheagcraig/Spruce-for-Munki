879d72ed752156976021dcb2cd01f46d64c12f2d
Shea Craig <sheagcraig@gmail.com>
2015-11-18 14:44:12 -0500

    You lack discipline.
    - Got tests passing and then went crazy just finishing it. Ish.

--- build_configurations.py
+++ build_configurations.py
@@ -1,9 +1,65 @@
 #!/usr/bin/python
 
 
+import argparse
+import os
+
 import FoundationPlist
 
 
+def main():
+    args = get_argument_parser().parse_args()
+    preferences = FoundationPlist.readPlist(os.path.expanduser(
+        "~/Library/Preferences/com.github.sheagcraig.build_configurations.plist"))
+    template = preferences.get("munki_template",
+                               "ManagedInstallsTemplate.mobileconfig")
+    manifests = preferences.get("manifests", [])
+    catalogs = preferences.get("catalogs", [])
+    if manifests and catalogs:
+        configs = get_permutations(manifests, catalogs)
+        for config in configs:
+            output_munki_config(config, args)
+
+
+def get_argument_parser():
+    """Create our argument parser."""
+    description = ("Build configuration profiles with all permutations of "
+                   "configured manifests and catalogs.")
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("version", help="Version to use in mobileconfig "
+                        "filename")
+    parser.add_argument("-o", "--output_dir",
+                        help="Directory to write output into.", default=".")
+    return parser
+
+
 def get_permutations(manifests, catalogs):
-    return ((manifest, catalog) for manifest in manifests for catalog in
-            catalogs)
+    return ((manifest, catalog.title()) for manifest in manifests for catalog
+            in catalogs)
+
+def output_munki_config(config, args):
+    version = args.version
+    output_dir = args.output_dir
+    config_plist = build_munki_config(config)
+    basename = "ManagedInstalls-{}-{}.mobileconfig".format(
+        "-".join(config), version)
+    path = os.path.join(output_dir, basename)
+    write_config_to(config_plist, path)
+
+
+def build_munki_config(config_type):
+    plist = FoundationPlist.readPlist("ManagedInstallsTemplate.mobileconfig")
+    plist["PayloadContent"][0]["PayloadContent"]["ManagedInstalls"]["Forced"][0]["mcx_preference_settings"][
+        "ClientIdentifier"] = ( "-".join(config_type))
+    return plist
+
+
+def write_config_to(plist, path):
+    try:
+        FoundationPlist.writePlist(plist, os.path.expanduser(path))
+    except FoundationPlist.NSPropertyListWriteException as error:
+        print error.message
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_build_configurations.py
+++ tests/test_build_configurations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+import FoundationPlist
 from nose.tools import *
 
 import build_configurations
@@ -8,12 +9,25 @@ import build_configurations
 
 class TestBuildConfigurations(object):
 
+    def setUp(self):
+        self.manifests = ("IT", "General", "JMP")
+        self.catalogs = ("testing", "phase1", "phase2")
+
     def test_get_permutations(self):
-        manifests = ("IT", "General", "JMP")
-        catalogs = ("testing", "phase1", "phase2")
-        expecteds = [(manifest, catalog) for manifest in manifests for catalog
-                     in catalogs]
+        expecteds = [(manifest, catalog.title()) for manifest in self.manifests
+                     for catalog in self.catalogs]
         results = list(build_configurations.get_permutations(
-            manifests, catalogs))
+            self.manifests, self.catalogs))
         for expected in expecteds:
             assert_in(expected, results)
+
+    def test_build_munki_config(self):
+        expected_plist = FoundationPlist.readPlist(
+            "tests/resources/Expected.plist")
+        result = build_configurations.build_munki_config(("IT", "Testing"))
+        test_values = ("ClientIdentifier",)
+        for key in test_values:
+            assert_equal(
+                expected_plist["PayloadContent"][0]["PayloadContent"]["ManagedInstalls"]["Forced"][0]["mcx_preference_settings"][key],
+                result["PayloadContent"][0]["PayloadContent"]["ManagedInstalls"]["Forced"][0]["mcx_preference_settings"][key])
+
