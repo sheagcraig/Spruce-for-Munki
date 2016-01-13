6ba37f28db622ff860a2b94fb714f2f8c3c3754e
Shea Craig <sheagcraig@gmail.com>
2016-01-13 10:59:07 -0500

    Add license. Remove commented out code. Pylint.

--- recategorizer.py
+++ recategorizer.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+# Copyright 2016 Shea G. Craig
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Report on and mass-edit the categories used to classify software
+on a Munki repository.
+"""
 
 
 import argparse
@@ -13,18 +32,9 @@ PKGINFO_EXTENSIONS = (".pkginfo", ".plist")
 
 
 def main():
+    """Handle arguments and execute commands."""
     args = get_argument_parser().parse_args()
     args.func(args)
-    # preferences = FoundationPlist.readPlist(os.path.expanduser(
-    #     "~/Library/Preferences/com.github.sheagcraig.build_configurations.plist"))
-    # template = preferences.get("munki_template",
-    #                            "ManagedInstallsTemplate.mobileconfig")
-    # manifests = preferences.get("manifests", [])
-    # catalogs = preferences.get("catalogs", [])
-    # if manifests and catalogs:
-    #     configs = get_permutations(manifests, catalogs)
-    #     for config in configs:
-    #         output_munki_config(config, args)
 
 
 def get_argument_parser():
@@ -37,7 +47,7 @@ def get_argument_parser():
     phelp = ("List all categories present in the repo, and the count of "
              "pkginfo files in each.")
     collect_parser = subparser.add_parser("categories", help=phelp)
-    collect_parser.set_defaults(func=categories)
+    collect_parser.set_defaults(func=run_categories)
 
     # Prepare arguments
     phelp = ("List all product names, sorted by category, as a plist. This "
@@ -48,34 +58,30 @@ def get_argument_parser():
     collect_parser = subparser.add_parser("prepare", help=phelp)
     collect_parser.set_defaults(func=prepare)
 
-    # parser.add_argument("", help=""
-    #                     "filename")
-    # parser.add_argument("-o", "--output_dir",
-    #                     help="Directory to write output into.", default=".")
     return parser
 
 
-def categories(args):
+def run_categories(_):
+    """Output all present categories and counts of their use."""
     all_catalog = get_all_catalog()
     all_categories = get_categories(all_catalog)
     categories = Counter(all_categories)
     if "" in categories:
         categories["BLANK CATEGORY"] = categories[""]
-        del(categories[""])
-    # with open("output.csv", "wb") as output_file:
-    #     writer = csv.writer(output_file)
-    #     writer.writerows(categories)
+        del categories[""]
     for category in sorted(categories):
         print "{}: {}".format(category.encode("utf-8"), categories[category])
 
 
 def get_all_catalog():
+    """Return the Munki 'all' catalog as a plist dict."""
     munki_repo = get_repo_path()
     all_path = os.path.join(munki_repo, "catalogs", "all")
     return FoundationPlist.readPlist(all_path)
 
 
 def get_repo_path():
+    """Get path to the munki repo according to munkiimport's prefs."""
     munkiimport_prefs = get_munkiimport_prefs()
     return munkiimport_prefs.get("repo_path")
 
@@ -85,14 +91,14 @@ def get_munkiimport_prefs():
         "~/Library/Preferences/com.googlecode.munki.munkiimport.plist"))
 
 
-def get_categories(all_catalog, filter=None):
-    if not filter:
-        filter = True
+def get_categories(all_catalog, filter_func=lambda x: True):
+    """Return a list of the category for each pkginfo in the repo."""
     return [pkginfo.get("category", "NO CATEGORY") for pkginfo in all_catalog
-            if filter(pkginfo)]
+            if filter_func(pkginfo)]
 
 
-def prepare(args):
+def prepare(_):
+    """Build a plist of categories and their products."""
     all_catalog = get_all_catalog()
     names = get_unique_names(all_catalog)
     names_by_category = defaultdict(list)
@@ -103,7 +109,8 @@ def prepare(args):
     output["Comment"] = help_text
 
     for name in names:
-        categories = get_categories(all_catalog, lambda n: n["name"] == name)
+        name_filter = lambda n: n["name"] == name  # pylint: disable=cell-var-from-loop
+        categories = get_categories(all_catalog, name_filter)
         most_frequent_category = Counter(categories).most_common(1)[0][0]
         names_by_category[most_frequent_category].append(name)
 
@@ -112,15 +119,17 @@ def prepare(args):
 
 
 def get_unique_names(all_catalog):
+    """Return a set of product names."""
     return {pkginfo.get("name", "*NO NAME*") for pkginfo in all_catalog}
 
 
 def build_pkginfo_cache(repo):
+    """Return a dict of all pkginfo files in the repo."""
     pkginfos = {}
     pkginfo_dir = os.path.join(repo, "pkgsinfo")
-    for dirpath, dirnames, filenames in os.walk(pkginfo_dir):
-        for file in filter(is_pkginfo, filenames):
-            path = os.path.join(dirpath, file)
+    for dirpath, _, filenames in os.walk(pkginfo_dir):
+        for ifile in filter(is_pkginfo, filenames):
+            path = os.path.join(dirpath, ifile)
             try:
                 pkginfo_file = FoundationPlist.readPlist(path)
             except FoundationPlist.FoundationPlistException:
@@ -133,34 +142,6 @@ def build_pkginfo_cache(repo):
 
 def is_pkginfo(candidate):
     return os.path.splitext(candidate)[-1].lower() in PKGINFO_EXTENSIONS
-
-
-# def get_permutations(manifests, catalogs):
-#     return ((manifest, catalog.title()) for manifest in manifests for catalog
-#             in catalogs)
-
-# def output_munki_config(config, args):
-#     version = args.version
-#     output_dir = args.output_dir
-#     config_plist = build_munki_config(config)
-#     basename = "ManagedInstalls-{}-{}.mobileconfig".format(
-#         "-".join(config), version)
-#     path = os.path.join(output_dir, basename)
-#     write_config_to(config_plist, path)
-
-
-# def build_munki_config(config_type):
-#     plist = FoundationPlist.readPlist("ManagedInstallsTemplate.mobileconfig")
-#     plist["PayloadContent"][0]["PayloadContent"]["ManagedInstalls"]["Forced"][0]["mcx_preference_settings"][
-#         "ClientIdentifier"] = ( "-".join(config_type))
-#     return plist
-
-
-# def write_config_to(plist, path):
-#     try:
-#         FoundationPlist.writePlist(plist, os.path.expanduser(path))
-#     except FoundationPlist.NSPropertyListWriteException as error:
-#         print error.message
 
 
 if __name__ == "__main__":
