321d15df989d8d33cf31e428d28cae0fc68fe68b
Shea Craig <sheagcraig@gmail.com>
2016-01-14 14:38:30 -0500

    Finish basic functionality.

--- deprecator.py
+++ deprecator.py
@@ -21,7 +21,10 @@ to a deprecated repository."""
 
 import argparse
 from collections import Counter, defaultdict
+import glob
 import os
+import shutil
+import sys
 from xml.sax.saxutils import escape
 
 import FoundationPlist
@@ -37,15 +40,32 @@ def main():
     cache = tools.build_pkginfo_cache(tools.get_repo_path())
 
     removals = get_files_to_remove(args, cache)
+    names = get_names_to_remove(removals, cache)
+
+    removal_type = "archived" if args.archive else "removed"
+    print_removals(removals, removal_type)
+    warn_about_multiple_pkg_references(removals, cache)
+
+    if not args.force:
+        response = raw_input("Are you sure you want to continue? (Y|N): ")
+        if response.upper() not in ("Y", "YES"):
+            sys.exit()
+
     if args.archive:
-        move_to_archive(removals)
+        move_to_archive(removals, args.archive)
     else:
         remove(removals)
+
+    remove_names_from_manifests(names)
 
 
 def get_argument_parser():
     """Create our argument parser."""
-    description = ("Remove unwanted products from a Munki repo.")
+    description = (
+        "Remove unwanted products from a Munki repo. Pkg and pkginfo files "
+        "will be removed, or optionally can be archived in an archive repo. "
+        "All products to be completely removed will then have their names "
+        "removed from all manifests.")
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument_group("Global Arguments")
@@ -67,9 +87,10 @@ def get_argument_parser():
 def get_files_to_remove(args, cache):
     """Build and return a list of files to remove."""
     removals = []
-    removals += get_removals_for_categories(args.category, cache)
-    removals += get_removals_for_names(args.name, cache)
-    print removals
+    if args.category:
+        removals += get_removals_for_categories(args.category, cache)
+    if args.name:
+        removals += get_removals_for_names(args.name, cache)
     return removals
 
 
@@ -85,128 +106,157 @@ def get_removals_for_categories(categories, cache):
                 pkg_removals.append(
                     os.path.join(pkg_prefix, plist["installer_item_location"]))
 
-    # Check for pkginfo files that are NOT to be removed which reference
-    # any pkgs to be removed and warn the user!
-    for path, plist in cache.items():
-        if (not path in pkginfo_removals and
-                plist.get("installer_item_location") in pkg_removals):
-            print ("WARNING: Package '{}' is targeted for removal, but has "
-                   "references in pkginfo '{}' which is not targeted for "
-                   "removal.".format(
-                       plist.get("intaller_item_location"), path))
-
     return pkginfo_removals + pkg_removals
 
 
 def get_removals_for_names(names, cache):
     """Get all pkginfo and pkg files to remove by name."""
-    return []
+    pkginfo_removals = []
+    pkg_removals = []
+    pkg_prefix = tools.get_pkg_path()
+    for path, plist in cache.items():
+        if plist.get("name") in names:
+            pkginfo_removals.append(path)
+            if plist.get("installer_item_location"):
+                pkg_removals.append(
+                    os.path.join(pkg_prefix, plist["installer_item_location"]))
+
+    return pkginfo_removals + pkg_removals
 
 
-def move_to_archive(removals):
+def get_names_to_remove(removals, cache):
+    """Return a set of all the 'name' values for pkginfos to remove."""
+    return {cache[path].get("name") for path in removals if path in cache}
+
+
+def print_removals(removals, removal_type):
+    """Pretty print the files to remove."""
+    print "Items to be {}".format(removal_type)
+    for item in sorted(removals):
+        print "\t{}".format(item)
+
+    print
+
+
+def warn_about_multiple_pkg_references(removals, cache):
+    """Alert user about possible pkg removal dependencies."""
+    # Check for pkginfo files that are NOT to be removed which reference
+    # any pkgs to be removed and warn the user!
+    for path, plist in cache.items():
+        if (not path in removals and
+                plist.get("installer_item_location") in removals):
+            print ("WARNING: Package '{}' is targeted for removal, but has "
+                   "references in pkginfo '{}' which is not targeted for "
+                   "removal.".format(
+                       plist.get("intaller_item_location"), path))
+
+
+def move_to_archive(removals, archive_path):
     """Move a list of files to an archive folder."""
-    pass
+    pkgs_folder = os.path.join(archive_path, "pkgs")
+    pkgsinfo_folder = os.path.join(archive_path, "pkgsinfo")
+    for folder in (pkgs_folder, pkgsinfo_folder):
+        make_folders(folder)
+
+    repo_prefix = tools.get_repo_path()
+    for item in removals:
+        archive_item = item.replace(repo_prefix, archive_path, 1)
+        make_folders(os.path.dirname(archive_item))
+        # TODO: Disabled until GO TIME.
+        shutil.move(item, archive_item)
+
+
+def make_folders(folder):
+    """Make all folders in path that are missing."""
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+        except OSError:
+            print ("Failed to create archive directory {}! "
+                    "Quitting.".format(folder))
+            sys.exit(1)
 
 
 def remove(removals):
     """Delete a list of files."""
-    pass
+    for item in removals:
+        try:
+            os.remove(item)
+        except OSError as error:
+            print ("Unable to remove {} with error: {}".format(
+                item, error.message))
 
 
-# def run_categories(args):
-#     """Output all present categories and counts of their use."""
-#     all_catalog = tools.get_all_catalog()
-#     if not args.category:
-#         get_categories_and_counts(all_catalog)
-#     else:
-#         get_categories_and_files(all_catalog, args.category)
+def remove_names_from_manifests(names):
+    """Remove names from all manifests."""
+    # Build a new cache post-removal. We haven't run makecatalogs, so
+    # we can't use the catalogs for this task.
+    repo_path = tools.get_repo_path()
+    manifests_path = os.path.join(repo_path, "manifests")
+
+    cache = tools.build_pkginfo_cache(repo_path)
+    remaining_names = {pkginfo.get("name") for pkginfo in cache.values()}
+    # Use set arithmetic to remove names that are still active in the
+    # repo from our removals set.
+    names_to_remove = names - remaining_names
+    import pdb;pdb.set_trace()
+
+    keys = ("managed_installs", "optional_installs", "managed_updates",
+            "managed_uninstalls")
+
+    for manifest_path in glob.glob(os.path.join(manifests_path, "*")):
+        changed = False
+        try:
+            manifest = FoundationPlist.readPlist(manifest_path)
+        except FoundationPlist.FoundationPlistException:
+            print "Error reading manifest {}".format(manifest_path)
+            next
+        print "Looking for name removals in {}".format(manifest_path)
+
+        for key in keys:
+            product_array = manifest.get(key)
+            if product_array:
+                changes = handle_name_removal(product_array, names_to_remove,
+                                              key)
+                if changes:
+                    changed = True
+
+        # TODO: This can be refactored out as it's a duplicate, just
+        # one layer deeper in the manifest.
+        if "conditional_items" in manifest:
+            conditionals = manifest["conditional_items"]
+            for conditional in conditionals:
+                for key in keys:
+                    product_array = conditional.get(key)
+                    if product_array:
+                        changes = handle_name_removal(
+                            product_array, names_to_remove,
+                            "conditional " + key)
+                        if changes:
+                            changed = True
+
+        if changed:
+            FoundationPlist.writePlist(manifest, manifest_path)
 
 
-# def get_categories_and_counts(all_catalog):
-#     all_categories = tools.get_categories(all_catalog)
-#     categories = Counter(all_categories)
-#     if "" in categories:
-#         if NO_CATEGORY not in categories:
-#             categories[NO_CATEGORY] = 0
-#         categories[NO_CATEGORY] += categories[""]
-#         del categories[""]
-#     for category in sorted(categories):
-#         print "{}: {}".format(category.encode("utf-8"), categories[category])
+def handle_name_removal(product_array, names_to_remove, key):
+    removals = []
+    changes = False
+    for item in product_array:
+        if item in names_to_remove:
+            print "\tRemoving {} from {}".format(item, key)
+            removals.append(item)
+        elif (item.startswith(tuple(names_to_remove)) and not
+                item.endswith(tuple(names_to_remove))):
+            print ("\tDeprecator found item {} that may match a "
+                    "name to remove, but the length is wrong. "
+                    "Please remove manually if required!").format(
+                        item)
+    for item in removals:
+        product_array.remove(item)
+        changes = True
 
-
-# def get_categories_and_files(all_catalog, categories):
-#     cache = tools.build_pkginfo_cache(tools.get_repo_path())
-#     output = defaultdict(list)
-
-#     # Output only those pkginfos which are in the requested categories.
-#     for path, plist in cache.items():
-#         category = plist.get("category")
-#         if category in categories:
-#             output[category].append((plist.get("name"), path))
-
-#     for key, val in output.items():
-#         print "Category: {}".format(key)
-#         for entry in sorted(val):
-#             print "\t{}, {}".format(*entry)
-
-
-# def prepare(_):
-#     """Build a plist of categories and their products."""
-#     all_catalog = tools.get_all_catalog()
-#     names = tools.get_unique_names(all_catalog)
-#     names_by_category = defaultdict(list)
-
-#     output = {}
-#     with open("recategorizer_help.txt") as ifile:
-#         help_text = escape(ifile.read())
-#     output["Comment"] = help_text
-
-#     for name in names:
-#         name_filter = lambda n: n["name"] == name  # pylint: disable=cell-var-from-loop
-#         categories = tools.get_categories(all_catalog, name_filter)
-#         most_frequent_category = Counter(categories).most_common(1)[0][0]
-#         if most_frequent_category == "":
-#             most_frequent_category = NO_CATEGORY
-#         names_by_category[most_frequent_category].append(name)
-
-#     output.update(names_by_category)
-#     print FoundationPlist.writePlistToString(output)
-
-
-# def update(args):
-#     """Update product pkginfo files to reflect specified categories."""
-#     changes = FoundationPlist.readPlist(os.path.expanduser(args.plist))
-
-#     # Remove the comment that we instert into the output of prepare.
-#     if "Comment" in changes:
-#         del changes["Comment"]
-
-#     products = {product for change_group in changes.values() for product in
-#                 change_group}
-
-#     cache = tools.build_pkginfo_cache(tools.get_repo_path())
-
-#     # Update only those pkginfos which need changes applied.
-#     for path, plist in cache.items():
-#         name = plist.get("name")
-#         if name in products:
-#             new_category = get_category_for_name(name, changes)
-#             if new_category == NO_CATEGORY:
-#                 new_category = ""
-#             category = plist.get("category")
-
-#             if new_category != category:
-#                 plist["category"] = new_category
-#                 FoundationPlist.writePlist(plist, path)
-#                 print "Pkginfo {} category set to {}.".format(
-#                      path, new_category if new_category else "''")
-
-
-# def get_category_for_name(name, changes):
-#     """Get the desired category for product 'name'."""
-#     for category in changes:
-#         if name in changes[category]:
-#             return category
+    return changes
 
 
 if __name__ == "__main__":
