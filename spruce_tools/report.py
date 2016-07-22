b6951fd711c2f91a01c5ca8be16d491ef1be0d31
Shea Craig <sheagcraig@gmail.com>
2016-07-22 10:52:25 -0400

    Remove Repo class and helper funcs from report, add sorting

--- spruce_tools/report.py
+++ spruce_tools/report.py
@@ -24,295 +24,15 @@ from xml.parsers.expat import ExpatError
 import sys
 
 import cruftmoji
+from repo import Repo
 from robo_print import robo_print, LogLevel
 import tools
 import FoundationPlist
 
 
-PKGINFO_EXTENSIONS = (".pkginfo", ".plist")
 IGNORED_FILES = ('.DS_Store',)
-# Apple uses the IEEE/ISO Megabyte, so we will too.
-# (Munki uses Mebibytes).
-KILOBYTE = 1000
-MEGABYTE = KILOBYTE ** 2
-GIGABYTE = KILOBYTE ** 3
 
 
-class Repo(object):
-
-    def __init__(self, pkgsinfo):
-        self.applications = {}
-        for path, pkginfo in pkgsinfo.items():
-            item = ApplicationVersion(path, pkginfo)
-            name = item.name
-            if name not in self:
-                self[name] = Application(name, (item,))
-            else:
-                self[name].add(item)
-
-        for app in self.applications.values():
-            app.add_dependencies(self)
-
-    def get_used_items(self, manifest_items, num_to_save, catalogs=None):
-        used = set()
-        for manifest_item in manifest_items:
-            # TODO: Add parameter for os version support.
-            # Support 10.9-10.12
-            for major in xrange(9, 13):
-                for minor in xrange(0, 10):
-                    test_version = "10.{}.{}".format(major, minor)
-                    used.update(self.used_by_for_os(
-                        manifest_item, self, test_version, num_to_save, used,
-                        catalogs))
-
-        return used
-
-    def used_by_for_os(self, name, repo, os_version, keep, used=None,
-                       catalogs=None):
-        if not used:
-            used = set()
-        name, version = split_name_from_version(name)
-        if not name in repo:
-            robo_print("'{}' does not exist in repo, but is specified in a "
-                       "manifest.".format(name), LogLevel.WARNING)
-            return used
-        else:
-            app = repo[name]
-
-        if version:
-            if version not in app:
-                robo_print("'{}-{}' does not exist in repo, but is specified "
-                           "in a manifest.".format(name, version),
-                           LogLevel.WARNING)
-                return used
-            else:
-                candidates = (app[version],)
-        else:
-            candidates = app
-
-        count = 0
-        test_version = LooseVersion(os_version)
-
-        for item in candidates:
-            min_version = LooseVersion(item.min_version or "10.4.0")
-            max_version = LooseVersion(item.max_version or "10.12.99")
-
-            if (test_version >= min_version and
-                test_version <= max_version and
-                self.meets_catalog_requirements(item, catalogs)):
-                if item in used:
-                    continue
-                else:
-                    used.add(item)
-                    count += 1
-                for required in item.requires:
-                    if isinstance(required, Application):
-                        used = self.used_by_for_os(
-                            required.name, repo, os_version, keep, used)
-                    elif required in used:
-                        continue
-                    else:
-                        used = self.used_by_for_os(
-                            required.name, repo, os_version, keep, used)
-
-                for update in item.updates:
-                    if update in used:
-                        continue
-                    else:
-                        used = self.used_by_for_os(
-                            update.name, repo, os_version, keep, used)
-
-            if count == keep:
-                return used
-
-        if len(used) == 0:
-            print ("Warning: Zero items were found that fit this OS "
-                   "requirement.")
-        return used
-
-    def meets_catalog_requirements(self, item, catalogs):
-        if catalogs:
-            return any(
-                cat in catalogs for cat in item.pkginfo.get(catalogs, []))
-        else:
-            return True
-
-    def __getitem__(self, name):
-        return self.applications[name]
-
-    def __setitem__(self, name, value):
-        self.applications[name] = value
-
-    def __contains__(self, name):
-        return name in self.applications
-
-
-class Application(object):
-    """Describes a software item from the Munki repo.
-
-    This object acts as a container for managing instances of the
-    ApplicationVersion class.
-    """
-    def __init__(self, name, app_versions=None):
-        self.name = name
-        self._app_versions = []
-        if app_versions:
-            for app_version in app_versions:
-                self.add(app_version)
-
-    def __iter__(self):
-        """Return an iterator from newest to oldest version."""
-        self._app_versions.sort(reverse=True)
-        for app_version in self._app_versions:
-            yield app_version
-
-    def __len__(self):
-        return len(self._app_versions)
-
-    def __repr__(self):
-        return "{}:\n{}".format(self.name, "\n".join(
-            str(item) for item in self))
-
-    def __getitem__(self, version):
-        search = [item for item in self if item.version == version]
-        if len(search) == 1:
-            result = search[0]
-        elif len(search) > 1:
-            robo_print("More than one pkg with version '{}'!".format(
-                version), LogLevel.WARNING)
-            result = search[0]
-        else:
-            raise KeyError(version)
-        return result
-
-    def __contains__(self, version):
-        return any(version == item.version for item in self)
-
-    def add(self, app_version):
-        if not isinstance(app_version, ApplicationVersion):
-            raise ValueError("Unsupported argument type.")
-        self._app_versions.append(app_version)
-        self._app_versions.sort()
-
-    def add_dependencies(self, repo):
-        for version in self._app_versions:
-            version.add_dependencies(repo)
-
-    def get_newest(self, num):
-        self._app_versions.sort(reverse=True)
-        if num > len(self._app_versions):
-            num = len(self._app_versions)
-
-        return self._app_versions[0:num]
-
-
-class ApplicationVersion(object):
-
-    def __init__(self, pkginfo_path, pkginfo):
-        self.pkginfo_path = pkginfo_path
-        self.pkg_path = pkginfo.get("installer_item_location")
-        self.name = pkginfo.get("name")
-        self.min_version = pkginfo.get("minimum_os_version")
-        self.max_version = pkginfo.get("maximum_os_version")
-        self.version = pkginfo.get("version")
-        self.pkginfo = pkginfo
-        if self.pkg_path:
-            # TODO: For now, let it raise an exception if pkg is missing
-            size = os.stat(
-                os.path.join(tools.get_pkg_path(), self.pkg_path)).st_size
-        else:
-            size = 0
-        self.size = size
-        self.requires = []
-        self.required_by = []
-        self.update_for = []
-        self.updates = []
-
-    def _human_readable_size(self):
-        if self.size >= GIGABYTE:
-            size = "{:,.2f}G".format(float(self.size) / GIGABYTE)
-        elif self.size < GIGABYTE and self.size >= MEGABYTE:
-            size = "{:,.2f}M".format(float(self.size) / MEGABYTE)
-        elif self.size < MEGABYTE:
-            size = "{}K".format(self.size)
-        return size
-
-    def __repr__(self):
-        head_fmt = "{} {} ({} - {}): {}"
-
-        output = head_fmt.format(self.name, self.version, self.min_version,
-                                 self.max_version, self._human_readable_size())
-        if self.requires:
-            output += " Requires: {}".format(len(self.requires))
-        if self.update_for:
-            output += " update_for: {}".format(len(self.update_for))
-        if self.updates:
-            output += " Updates: {}".format(len(self.updates))
-
-        return output
-
-    def __cmp__(self, other):
-        version = LooseVersion(self.version)
-        other = LooseVersion(other.version)
-        if version < other:
-            return -1
-        elif version == other:
-            return 0
-        else:
-            return 1
-
-    def add_dependencies(self, repo):
-        for required_name in self.pkginfo.get("requires", []):
-            name, version = split_name_from_version(required_name)
-            # Item requires a specific version.
-            if name not in repo:
-                robo_print("'{}' requires '{}', but there is not an item with "
-                           "that name.".format(self.name, name),
-                           LogLevel.WARNING)
-                continue
-
-            if version:
-                if version not in repo[name]:
-                    robo_print("'{}' requires '{}-{}', but there is not an "
-                               "item with that version.".format(
-                                   self.name, name, version), LogLevel.WARNING)
-                    continue
-                else:
-                    self.requires.append(repo[name][version])
-                    repo[name][version].required_by.append(self)
-            else:
-                self.requires.append(repo[name])
-                for item in repo[name]:
-                    item.required_by.append(self)
-
-        for update_for_name in self.pkginfo.get("update_for", []):
-            name, version = split_name_from_version(update_for_name)
-            if name not in repo:
-                robo_print("'{}' is an update for '{}', but that item does "
-                           "not exist.".format(self.name, name),
-                           LogLevel.WARNING)
-                continue
-
-            # Update is for a specific version.
-            if version:
-                if version not in repo[name]:
-                    robo_print("'{}' is an update for '{}-{}', but there is "
-                               "not an item with that version.".format(
-                                   self.name, name, version), LogLevel.WARNING)
-                    continue
-                else:
-                    repo[name][version].add_update(self)
-                    self.update_for.append(repo[name][version])
-            else:
-                for item in repo[name]:
-                    item.add_update(self)
-                    self.update_for.append(item)
-
-    def add_update(self, update):
-        self.updates.append(update)
-
-
-# TODO: Sort items in Report.
 class Report(object):
     """Encapsulates behavior of a Spruce Report.
 
@@ -348,7 +68,6 @@ class Report(object):
         if self.items or self.metadata:
             if self.items_keys:
                 for key, reverse in reversed(self.items_keys):
-                    import pdb; pdb.set_trace()
                     self.items.sort(key=itemgetter(key), reverse=reverse)
             self._print_section("items")
             print
@@ -463,6 +182,7 @@ class OutOfDateReport(Report):
 
 class PathIssuesReport(Report):
     name = "Pkginfos with Case-Sensitive Pkg Path Errors"
+    items_keys = (("name", False),)
     items_order = ["name", "path"]
 
     def run_report(self, repo_data):
@@ -491,6 +211,7 @@ class PathIssuesReport(Report):
 
 class MissingInstallerReport(Report):
     name = "Pkginfos with Missing Installer Items"
+    items_keys = (("name", False),)
     items_order = ["name", "path"]
 
     def run_report(self, repo_data):
@@ -508,6 +229,7 @@ class MissingInstallerReport(Report):
 
 class OrphanedInstallerReport(Report):
     name = "Pkgs with no Referring Pkginfo"
+    items_keys = (("path", False),)
     items_order = ["path"]
 
     def run_report(self, repo_data):
@@ -596,6 +318,7 @@ class NoUsageReport(Report):
 
 class PkgsinfoWithErrorsReport(Report):
     name = "Pkgsinfo with Syntax Errors"
+    items_keys = (("path", False),)
     items_order = ["path"]
 
     def run_report(self, errors):
@@ -603,6 +326,7 @@ class PkgsinfoWithErrorsReport(Report):
             self.items.append({"path": key, "error": value})
 
 
+# TODO: Add to other reports.
 class UnusedDiskUsageReport(Report):
     name = "Unused / Out Of Date Item Disk Usage"
 
@@ -622,6 +346,8 @@ class UnusedDiskUsageReport(Report):
 
 class SimpleConditionReport(Report):
     """Report Subclass for simple reports."""
+    items_keys = (("name", False), ("version", True))
+    items_order = ["name", "path"]
 
     def run_report(self, repo_data):
         self.items = self.get_info(self.conditions, repo_data["pkgsinfo"])
@@ -673,9 +399,9 @@ def run_reports(args):
     report_results.append(NoUsageReport(expanded_cache))
     # Add the results of the last two reports together to determine
     # wasted disk space.
-    expanded_cache["unused_items"] = [item for report in report_results[-2:]
-                                      for item in report.items]
-    report_results.append(UnusedDiskUsageReport(expanded_cache))
+    # expanded_cache["unused_items"] = [item for report in report_results[-2:]
+    #                                   for item in report.items]
+    # report_results.append(UnusedDiskUsageReport(expanded_cache))
     report_results.append(UnattendedTestingReport(expanded_cache))
     report_results.append(UnattendedProdReport(expanded_cache))
     report_results.append(ForceInstallTestingReport(expanded_cache))
@@ -724,8 +450,13 @@ def get_manifests(munki_repo):
         for filename in filenames:
             if filename not in IGNORED_FILES:
                 manifest_filename = os.path.join(dirpath, filename)
-                manifests[manifest_filename] = FoundationPlist.readPlist(
+                try:
+                    manifests[manifest_filename] = FoundationPlist.readPlist(
                         manifest_filename)
+                except FoundationPlist.NSPropertyListSerializationException as err:
+                    robo_print("Failed to open manifest '{}' with error "
+                               "'{}'.".format(manifest_filename, err.message),
+                               LogLevel.WARNING)
     return manifests
 
 
@@ -807,25 +538,6 @@ def get_explicitly_used_items(manifests, pkgsinfo):
                 if items:
                     used_items.update(items)
 
-    # If `name` is used, then `name-version` is implicitly used as well.
-    # for pkginfo in pkgsinfo.values():
-    #     name = pkginfo.get("name")
-    #     version = pkginfo.get("version")
-    #     if name in used_items:
-    #         used_items.add("{}-{}".format(name, version))
-
-    #     if name in used_items or "{}-{}".format(name, version) in used_items:
-    #         requires = pkginfo.get("requires")
-    #         if requires:
-    #             used_items.update(requires)
-
-    # added_items = add_update_fors(pkgsinfo, used_items)
-    # A pkginfo that is not used could be an update_for something that
-    # is. If that update_for pkginfo is added, another pkginfo may now
-    # potentially be an update_for it, so loop until no items are added.
-    # while added_items is True:
-    #     added_items = add_update_fors(pkgsinfo, used_items)
-
     return used_items
 
 
@@ -859,26 +571,6 @@ def add_update_fors(pkgsinfo, used_items):
                     result = True
 
     return result
-
-
-def split_name_from_version(name_string):
-    """Splits a string into the name and version number.
-    Name and version must be seperated with a hyphen ('-')
-    or double hyphen ('--').
-    'TextWrangler-2.3b1' becomes ('TextWrangler', '2.3b1')
-    'AdobePhotoshopCS3--11.2.1' becomes ('AdobePhotoshopCS3', '11.2.1')
-    'MicrosoftOffice2008-12.2.1' becomes ('MicrosoftOffice2008', '12.2.1')
-    """
-    # This code comes from Munki.
-    for delim in ('--', '-'):
-        if name_string.count(delim) > 0:
-            chunks = name_string.split(delim)
-            vers = chunks.pop()
-            name = delim.join(chunks)
-            if vers[0] in '0123456789':
-                return (name, vers)
-
-    return (name_string, '')
 
 
 if __name__ == "__main__":
